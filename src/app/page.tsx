d140403198fa38325d5143678981489beb3b402e
Mohamed Ashraf <mashraf@road9media.com>
2024-12-14 14:00:36 +0200

    build: added docker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-	const _name = "John doe";
-
 	return (
 		<div className={styles.page}>
 			<main className={styles.main}>
